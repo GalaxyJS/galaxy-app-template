de59aba6585f0e49db87bbafcecb2e95fc4f485f
Eeliya <eeliya.rasta@gmail.com>
2023-01-30 20:40:03 +0100

    Improvements.

--- src/ui/main.js
+++ src/ui/main.js
@@ -2,9 +2,11 @@
 const view = Scope.import('galaxy/view');
 
 view.container.node.innerHTML = '';
+
 view.components({
   'galaxy-welcome': Scope.import('ui/components/welcome.component.js')
-})
+});
+
 view.blueprint([
   {
     tag: 'galaxy-welcome'
